ce2c7a20278c533067786ac1a9b6e5ea6905d9d6
Kovali <kovali94738@icloud.com>
2023-02-03 17:36:45 +0700

    Initial Commit

---
+++ Money don't sleep/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Money don't sleep
+//
+//  Created by Григорий Ковалев on 03.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Money don't sleep/Money_don_t_sleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_don_t_sleepApp.swift
+//  Money don't sleep
+//
+//  Created by Григорий Ковалев on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Money_don_t_sleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
